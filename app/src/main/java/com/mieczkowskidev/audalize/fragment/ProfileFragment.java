3c59f78cc6586621036b5d89cfffd27287f0b6fe
Patryk Mieczkowski <mieczkowskidev@gmail.com>
2016-02-11 20:03:59 +0100

    logout and delete user methods

--- app/src/main/java/com/mieczkowskidev/audalize/fragment/ProfileFragment.java
+++ app/src/main/java/com/mieczkowskidev/audalize/fragment/ProfileFragment.java
@@ -1,5 +1,8 @@
 package com.mieczkowskidev.audalize.fragment;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -7,19 +10,24 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.mieczkowskidev.audalize.API.RestAPI;
 import com.mieczkowskidev.audalize.API.RestClient;
+import com.mieczkowskidev.audalize.LoginActivity;
 import com.mieczkowskidev.audalize.R;
 import com.mieczkowskidev.audalize.model.DataProfile;
 import com.mieczkowskidev.audalize.utils.LoginManager;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import rx.Observer;
+import butterknife.OnClick;
+import retrofit.client.Response;
+import rx.Subscriber;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
 
 /**
  * Created by Patryk Mieczkowski on 06.02.16.
@@ -33,9 +41,19 @@ public class ProfileFragment extends Fragment {
     }
 
     @Bind(R.id.textViewName)
-    TextView textView;
+    TextView usernameText;
 
-    Subscription subscriptionProfile;
+    @Bind(R.id.textViewMail)
+    TextView mailText;
+
+    @Bind(R.id.logout_button)
+    Button logoutButton;
+
+    @Bind(R.id.delete_button)
+    Button deleteButton;
+
+    Subscription subscriptionProfile, subscriptionLogout, subscriptionDelete;
+    RestAPI restAPI;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -55,19 +73,23 @@ public class ProfileFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-
+        createRestClient();
         getProfileInfo();
+    }
+
+    private void createRestClient() {
+        Log.d(TAG, "createRestClient()");
+
+        RestClient restClient = new RestClient();
+        restAPI = restClient.getRestAdapter().create(RestAPI.class);
     }
 
     private void getProfileInfo() {
         Log.d(TAG, "getProfileInfo()");
 
-        RestClient restClient = new RestClient();
-        RestAPI restAPI = restClient.getRestAdapter().create(RestAPI.class);
-
         subscriptionProfile = restAPI.getProfile(LoginManager.getTokenFromShared(getActivity()))
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<DataProfile>() {
+                .subscribe(new Subscriber<DataProfile>() {
                     @Override
                     public void onCompleted() {
                         Log.d(TAG, "onCompleted()");
@@ -82,19 +104,117 @@ public class ProfileFragment extends Fragment {
                     @Override
                     public void onNext(DataProfile dataProfile) {
                         Log.d(TAG, "onNext DataProfile");
-                        textView.setText(dataProfile.getUsername());
+                        usernameText.setText(dataProfile.getUsername());
+                        mailText.setText(dataProfile.getEmail());
+                    }
+                });
+    }
+
+    @OnClick(R.id.logout_button)
+    public void logoutUserDialog() {
+        Log.d(TAG, "logoutUserDialog()");
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("Are you sure you want to logout?")
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                })
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        logoutUser();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    private void logoutUser() {
+        Log.d(TAG, "logoutUser()");
+
+        subscriptionLogout = restAPI.logoutUser(LoginManager.getTokenFromShared(getActivity()))
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<Response>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.d(TAG, "onCompleted()");
+                        Intent intent = new Intent(getActivity(), LoginActivity.class);
+                        startActivity(intent);
+                        getActivity().finish();
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e(TAG, "onError() " + e.getMessage());
+                    }
+
+                    @Override
+                    public void onNext(Response response) {
+                        Log.d(TAG, "onNext()");
+
+                    }
+                });
+    }
+
+    @OnClick(R.id.delete_button)
+    public void deleteUserDialog() {
+        Log.d(TAG, "deleteUserDialog()");
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("Are you sure you want to delete an account and all the erase all the data from server?")
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                })
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        deleteUser();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    private void deleteUser() {
+        Log.d(TAG, "deleteUser()");
+
+        subscriptionDelete = restAPI.unregisterUser(LoginManager.getTokenFromShared(getActivity()))
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<Response>() {
+                    @Override
+                    public void call(Response response) {
+                        Log.d(TAG, "call() " + response.getReason());
+                        Intent intent = new Intent(getActivity(), LoginActivity.class);
+                        startActivity(intent);
+                        getActivity().finish();
+                    }
+                }, new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable throwable) {
+                        Log.e(TAG, "call" + throwable.getMessage());
                     }
                 });
     }
 
     @Override
     public void onDestroy() {
+        Log.d(TAG, "onDestroy() ProfileFragment");
         super.onDestroy();
 
-        if (subscriptionProfile != null) {
-            if (!subscriptionProfile.isUnsubscribed()) {
-                subscriptionProfile.unsubscribe();
-            }
+        if (subscriptionProfile != null && !subscriptionProfile.isUnsubscribed()) {
+            subscriptionProfile.unsubscribe();
+        }
+
+        if (subscriptionLogout != null && !subscriptionLogout.isUnsubscribed()) {
+            subscriptionLogout.unsubscribe();
+        }
+
+        if (subscriptionDelete != null && !subscriptionDelete.isUnsubscribed()) {
+            subscriptionDelete.unsubscribe();
         }
     }
 }
